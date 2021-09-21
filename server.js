02226270b910ae861855d9bf1b60a0e0dc8a34c7
claudemir cardoso da silva <claudemircardoso70@gmail.com>
2021-09-21 10:36:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get ('/', (req, res) => {
-    res.status(200).send({ message: 'Alo Mundo'});
+app.get ('/ ola', (req, res) => {
+    res.status(200).send({ message: 'Alo Mundo agora rota olá'});
 })
 
 app.listen(3001,() => {
